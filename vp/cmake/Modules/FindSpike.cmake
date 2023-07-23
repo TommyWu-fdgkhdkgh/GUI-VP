c5e2bc9b78884ca6915ec874fe8f02a7b1cdcd1e
tommy.wu <fdgkhdkgh@gmail.com>
2023-07-23 21:38:23 +0800

    Add linux-spike platform.
    Signed-off-by: tommy.wu <fdgkhdkgh@gmail.com>

---
+++ vp/cmake/Modules/FindSpike.cmake
@@ -0,0 +1,5 @@
+set(Spike_DIR "${CMAKE_SOURCE_DIR}/dependencies/spike-fdgk")
+
+set(Spike_INCLUDE_DIRS ${Spike_DIR}/include )
+set(Spike_INCLUDE_DIR  ${Spike_INCLUDE_DIRS} )
+set(Spike_LIBRARIES    ${Spike_DIR}/lib/libriscv.a )
